8d85c579d4902a12f4a6fa5fc240383a1983618a
obsession.oo <132919105+howard-liang-B@users.noreply.github.com>
2024-03-02 16:24:23 +0800

    Update and rename predict_seg.py to predict.py

--- predict.py
+++ predict.py
@@ -7,15 +7,15 @@ ultralytics.checks()
 
 from ultralytics import YOLO
 
-model = YOLO('C:\\Users\\user\\Desktop\\Yolov8 Project\\Yolov8_segmentation\\exp_013\\weights\\best.pt')
+model = YOLO('path/your/custom/model/paht') # yolov8n.pt、yolov8s.pt、yolov8n-seg.pt、yolov8s-world.pt ...
 
 results = model.predict(              # Step 2 : 使用 YOLOv8 模型進行物件偵測
-    source = 'datasets/robo_yolov8_v2/valid/images',        # - 指定代偵測影像子目錄
+    source = 'path/your/predict/images/folder',        # - 指定代偵測影像子目錄
     conf = 0.9,             # - 偵測信心水準門限值
     save = True,            # - 儲存偵測結果影像
     save_txt = True,        # - 儲存偵測結果物件位置(YOLO格式)
     save_conf = True,       # - 儲存物件偵測結果信心水準值
     save_crop = True,       # - 儲存擷取物件影像
     visualize = False,       # - 偵測過程特徵圖視覺化
-    augment = True
+    augment = True          # segmentation 會出錯
 )
